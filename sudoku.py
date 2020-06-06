fe912ef4ad59ccc9bfd92dcf03f72b9f023baed3
Sergi Orra <sergiorra17@gmail.com>
2020-06-06 10:39:41 +0200

    printing board

--- sudoku.py
+++ sudoku.py
@@ -4,18 +4,20 @@ from sudoku_utils import SudokuSolver
 def getBoardInput():
     board = []
     for i in range(1, 10):
-        row_values = input(f'Enter entries of row number {i}: ')
+        rowValues = input(f'Enter entries for row number {i}: ')
         try:
-            int(row_values)
+            int(rowValues)
         except:
             print('Values not valid')
             return None
-        board.append([int(i) for i in str(row_values)])
+        rowList = [int(i) for i in str(rowValues)]
+        board.append(rowList)
     return board
 
 
-initial_board = getBoardInput()
+print(f'###### Sudoku Solver ######')
+initialBoard = getBoardInput()
 
-if initial_board is not None:
-    sudoku_solver = SudokuSolver(initial_board)
-    print(sudoku_solver.board)
+if initialBoard is not None:
+    sudokuSolver = SudokuSolver(initialBoard)
+    sudokuSolver.showBoard()
--- sudoku_utils.py
+++ sudoku_utils.py
@@ -2,3 +2,15 @@ class SudokuSolver:
 
     def __init__(self, board):
         self.board = board
+
+    def showBoard(self):
+        for i in range(len(self.board)):
+            if i % 3 == 0 and i != 0:
+                print('---------------------')
+            for j in range(len(self.board)):
+                if j % 3 == 0 and j != 0:
+                    print('| ', end='')
+                if j == 8:
+                    print(self.board[i][j])
+                else:
+                    print(f'{self.board[i][j]} ', end='')
